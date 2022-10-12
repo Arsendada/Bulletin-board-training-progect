f1b05ccbb86d7306c7107b67ed840c318097a8ab
Arsen <arsendada@mail.ru>
2022-10-12 15:04:31 +0300

    Добавлены функции detail и by_rubric, под них реализованы html шаблоны

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from .views import *
 app_name = 'main'
 urlpatterns = [
     path('<int:pk>/', by_rubric, name='by_rubric'),
+    path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
     path('<str:page>', other_page, name='other'),
     path('', index, name='index'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
