bfd3a54f1695037349c50d1936b8a76c3a171574
Arsen <arsendada@mail.ru>
2022-10-21 15:05:19 +0300

    изменен файл utiliries.py

--- main/utilities.py
+++ main/utilities.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from os.path import splitext
 
+from django.template.loader import render_to_string
+
 
 def get_timestamp_path(instance, filename):
     return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
+
+
